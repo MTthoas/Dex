dc9fff113be48386a3f2c7729f68e75a794f9007
MTthoas <mpecquery@myges.fr>
2023-12-24 00:32:50 +0100

    [fix] Cors issue and % var

--- api/main.go
+++ api/main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/MTthoas/dex/api/controllers"
+    "github.com/MTthoas/dex/api/controllers"
+    "github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
     app := fiber.New()
 
+    app.Use(cors.New(cors.Config{
+        AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+        AllowOrigins:     "*",
+        AllowCredentials: true,
+        AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+    }))
+
+
     app.Get("/", func(c *fiber.Ctx) error {
         return c.SendString("Hello, World!")
     })
 
-	app.Get("/coins", controllers.GetCoins)
+    app.Get("/api/coins", controllers.GetCoins)
 
-	
-
-    app.Listen(":3000")
+    app.Listen(":5555")
 }
