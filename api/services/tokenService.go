2344a6a01498b2e31708b88adea00776cb210647
Guiiz94 <92375190+Guiiz94@users.noreply.github.com>
2024-03-08 12:24:12 +0100

    Deleted useless files (#47)
    * [fix] change library deprecated #41
    
    * [fix] return data at JSON format #39
    
    * [fix] deleted useless files #38

--- api/services/tokenService.go
+++ api/services/tokenService.go
@@ -1,59 +1,55 @@
 package services
 
 import (
-	"github.com/MTthoas/dex/api/models"
-	"encoding/json"
-	"log"
-	"fmt"
-	"net/http"
-	"io/ioutil"
 	"bytes"
-	"os"
+	"encoding/json"
+	"fmt"
+	"github.com/MTthoas/dex/api/models"
 	"github.com/joho/godotenv"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 func goDotEnvVariable(key string) string {
 
 	// load .env file
 	err := godotenv.Load(".env")
-  
+
 	if err != nil {
-	  log.Fatalf("Error loading .env file")
+		log.Fatal("Error loading .env file")
 	}
-  
+
 	return os.Getenv(key)
-  }
-  
-func ScrapeEthereumTokens() []models.Token {
+}
+
+func ScrapeEthereumTokens() ([]models.Token, error) {
 
 	apiUrl := goDotEnvVariable("API_URL")
 
-    resp, err := http.Get(apiUrl)
-    if err != nil {	
-		fmt.Println("Erreur lors de la requête GET :", err)
-    }
-    defer resp.Body.Close()
-
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	resp, err := http.Get(apiUrl)
 	if err != nil {
-		log.Fatal("Erreur lors de la lecture de la réponse :", err)
+        log.Printf("Erreur lors de la requête GET : %v", err)
+        return nil, err 
+    }
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Erreur lors de la lecture de la réponse : %v", err)
 	}
 	bodyString := string(bodyBytes)
 	fmt.Println("Réponse brute de l'API :", bodyString)
 
 	// Réinitialiser le body pour le décodage JSON
-	resp.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+	resp.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
 
-    // Décoder le JSON reçu
-    var tokens []models.Token
+	// Décoder le JSON reçu
+	var tokens []models.Token
 	err = json.NewDecoder(resp.Body).Decode(&tokens)
 	if err != nil {
-		fmt.Println("Erreur lors du décodage JSON :", err)
+		log.Fatalf("Erreur lors de la lecture de la réponse : %v", err)
 	}
 
-
-    // Afficher les tokens récupérés pour le debug
-    fmt.Println("Tokens récupérés :", tokens)
-
-    return tokens
+	return tokens, nil
 }
