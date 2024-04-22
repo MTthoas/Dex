809ea6c7bee3bdd21bf65cedb7e58bc955af880e
Matteo <mrodrigues7@myges.fr>
2024-04-22 15:37:01 +0200

    merge with develop

---
+++ api/services/token.service.go
@@ -0,0 +1,55 @@
+package services
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/MTthoas/dex/api/models"
+	"github.com/joho/godotenv"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func ScrapeEthereumTokens() ([]models.Token, error) {
+
+	apiUrl := goDotEnvVariable("API_URL")
+
+	resp, err := http.Get(apiUrl)
+	if err != nil {
+        log.Printf("Erreur lors de la requête GET : %v", err)
+        return nil, err 
+    }
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Erreur lors de la lecture de la réponse : %v", err)
+	}
+	bodyString := string(bodyBytes)
+	fmt.Println("Réponse brute de l'API :", bodyString)
+
+	// Réinitialiser le body pour le décodage JSON
+	resp.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	// Décoder le JSON reçu
+	var tokens []models.Token
+	err = json.NewDecoder(resp.Body).Decode(&tokens)
+	if err != nil {
+		log.Fatalf("Erreur lors de la lecture de la réponse : %v", err)
+	}
+
+	return tokens, nil
+}
