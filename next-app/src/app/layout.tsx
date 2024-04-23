41d8555b8be00b8e85fa674e1bc1db11e123edaa
MtThoas <matthias.pecquery@gmail.com>
2024-04-23 21:21:15 +0200

    feat: layout with walletConnect

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -1,8 +1,11 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import type { Metadata } from "next";
+import { headers } from "next/headers";
 
-const inter = Inter({ subsets: ["latin"] });
+import { cookieToInitialState } from "wagmi";
+
+import { config } from "@/config";
+import Web3ModalProvider from "@/context";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,9 +17,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const initialState = cookieToInitialState(config, headers().get("cookie"));
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Web3ModalProvider initialState={initialState}>
+          {children}
+        </Web3ModalProvider>
+      </body>
     </html>
   );
 }
