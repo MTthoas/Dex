7bf80122da173d961f8137f268405bf5429e4989
Matteo <mrodrigues7@myges.fr>
2024-06-05 10:30:40 +0200

    add two fields on the staking model

--- api/models/staking.model.go
+++ api/models/staking.model.go
@@ -8,6 +8,8 @@ type Staking struct {
     StakingID   int       `json:"staking_id" gorm:"primary_key;auto_increment"`
     UserID      int       `json:"user_id" gorm:"index"`      // Foreign key to Users table
     AmountStaked float64  `json:"amount_staked"`
+	RewardDebt    float64   `json:"reward_debt"`
+    LastStakedTime time.Time `json:"last_staked_time"`
     StartDate   time.Time `json:"start_date"`
     EndDate     *time.Time `json:"end_date"`                 // Nullable
     Status      string    `json:"status"`                    // See if it's really usefull
