3ad8ce6dc3b44ebd57fd3d9ae23d4d2790fd575a
Matteo <mrodrigues7@myges.fr>
2024-07-10 22:17:45 +0200

    adding script to deploy token

---
+++ contracts/script/deployERC20.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/token/Token.sol";
+import "../src/token/LiquidityPoolToken.sol";
+import "../src/LiquidityPool.sol";
+import "../src/LiquidityPoolFactory.sol";
+
+contract deployERC20 is Script {
+    address admin;
+
+    function setUp() public {
+        admin = vm.envAddress("ADMIN_ADDRESS");
+    }
+
+    function run() external {
+        vm.startBroadcast(admin);
+        Token tokenA = new Token("TKA", "TOKENA", 24000000 * 10 ** 18);
+        vm.stopBroadcast();
+    }
+}
