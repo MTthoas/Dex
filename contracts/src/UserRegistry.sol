62bbfeb91ca923060927476139cddfefc8196995
Vincent Rainaud <vrainaud.pro@gmail.com>
2024-07-21 20:10:21 +0200

    AccessManaged -> AccessControl + Tests

--- contracts/src/UserRegistry.sol
+++ contracts/src/UserRegistry.sol
@@ -2,25 +2,28 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import "@openzeppelin/contracts-upgradeable/access/manager/AccessManagedUpgradeable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 /**
  * @title UserRegistry
  * @dev This contract manages user registration, authentication, and administration for a decentralized exchange (DEX) platform.
  */
-contract UserRegistry is AccessManagedUpgradeable {
+contract UserRegistry is ReentrancyGuard, AccessControl {
     using EnumerableSet for EnumerableSet.UintSet;
+    
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     // Set to store registered user IDs
     EnumerableSet.UintSet private registeredUserIds;
-
-    mapping(address => User) public users;
 
     struct User {
         uint256 id;
         string name;
         bool isBanned;
     }
+
+    mapping(address => User) public users;
 
     /**************************************************/
     /********************* Events *********************/
@@ -30,15 +33,21 @@ contract UserRegistry is AccessManagedUpgradeable {
     event UserBanned(address indexed userAddress, uint256 userId);
     event UserUnbanned(address indexed userAddress, uint256 userId);
 
-    constructor() {
-        _disableInitializers();
-    }
-
-    /**
-     * @notice Initializer of the contract
-     */
-    function initialize(address _initialAuthority) external initializer {
-        __AccessManaged_init(_initialAuthority);
+    /*
+    * @notice Initializes the contract with the provided admin addresses.
+    * @param _admin The address of the first admin.
+    * @param _admin2 The address of the second admin.
+    * @param _admin3 The address of the third admin.
+    */
+    constructor(
+        address _admin,
+        address _admin2,
+        address _admin3
+    ) {
+        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+        _grantRole(ADMIN_ROLE, _admin);
+        _grantRole(ADMIN_ROLE, _admin2);
+        _grantRole(ADMIN_ROLE, _admin3);
     }
 
     /***************************************************/
@@ -88,7 +97,7 @@ contract UserRegistry is AccessManagedUpgradeable {
      * @notice Registers a new user with the provided name.
      * @param _name The name of the user.
      */
-    function registerUser(string memory _name) public restricted {
+    function registerUser(string memory _name) public onlyRole(ADMIN_ROLE) nonReentrant returns (uint256) {
         require(users[msg.sender].id == 0, "User already registered");
 
         uint256 userId = registeredUserIds.length() + 1; // TODO: this is not working if we remove a user but can we remove a user ?
@@ -96,29 +105,35 @@ contract UserRegistry is AccessManagedUpgradeable {
         registeredUserIds.add(userId);
 
         emit UserRegistered(msg.sender, userId);
+
+        return userId;
     }
 
     /**
      * @notice Bans the user with the given address.
      * @param _userAddress The address of the user to ban.
      */
-    function banUser(address _userAddress) public restricted {
+    function banUser(address _userAddress) public onlyRole(ADMIN_ROLE) nonReentrant returns (uint256) {
         require(isRegisteredUser(_userAddress), "User not registered");
 
         users[_userAddress].isBanned = true;
         emit UserBanned(_userAddress, users[_userAddress].id);
+
+        return users[_userAddress].id;
     }
 
     /**
      * @notice Unbans the user with the given address.
      * @param _userAddress The address of the user to unban.
      */
-    function unbanUser(address _userAddress) public restricted {
+    function unbanUser(address _userAddress) public onlyRole(ADMIN_ROLE) nonReentrant returns (uint256) {
         require(isRegisteredUser(_userAddress), "User not registered");
         require(users[_userAddress].isBanned, "User not banned");
 
         users[_userAddress].isBanned = false;
         emit UserUnbanned(_userAddress, users[_userAddress].id);
+
+        return users[_userAddress].id;
     }
 
     /**
@@ -126,7 +141,7 @@ contract UserRegistry is AccessManagedUpgradeable {
      * @param _oldAddress The old address of the user.
      * @param _newAddress The new address of the user.
      */
-    function transferUserId(address _oldAddress, address _newAddress) public restricted {
+    function transferUserId(address _oldAddress, address _newAddress) public onlyRole(ADMIN_ROLE) nonReentrant returns (uint256) {
         require(isRegisteredUser(_oldAddress), "Old address not registered");
         require(!isRegisteredUser(_newAddress), "New address already registered");
 
@@ -136,5 +151,7 @@ contract UserRegistry is AccessManagedUpgradeable {
         users[_newAddress] = User(userId, users[_oldAddress].name, users[_oldAddress].isBanned);
 
         emit UserRegistered(_newAddress, userId);
+
+        return userId;
     }
 }
