4c591b138c8efa8bb9370ab1f50658de63baa762
MTthoas <matthias.pecquery@gmail.com>
2024-07-22 02:58:49 +0200

    fix: clean folders

--- api/models/response.model.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type ResponseStruct struct {
-		Success bool `json:"success"`
-		Timestamp int64 `json:"timestamp"`
-		AmountOfCoins int `json:"amount_of_coins"`
-		Tokens []Token `json:"tokens"`
-	}
