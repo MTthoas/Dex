b51b6cc4115e597a3ed7e2ad467a27d16ec10ed7
Vincent Rainaud <vrainaud.pro@gmail.com>
2024-07-22 18:13:39 +0200

    adding banned collumn for user table

--- api/models/user.model.go
+++ api/models/user.model.go
@@ -4,6 +4,7 @@ type User struct {
 	Base
 	Name    string `json:"name" gorm:"not null" example:"John Doe"`
 	Address string `json:"address" gorm:"unique" example:"0x123abc" validate:"required"`
+	Banned  bool   `json:"banned" gorm:"default:false" example:"false"`
 }
 
 func (user *User) TableName() string {
