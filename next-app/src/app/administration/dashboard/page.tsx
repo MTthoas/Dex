a6132b17cd607438ecb4366946a8993622ba6ed9
Vincent RAINAUD <65185673+Ronfflex@users.noreply.github.com>
2024-07-23 12:39:42 +0200

    Users table (#189)
    * Adding usersTable
    
    * Adding balance but with infinite loop on hook
    
    * Auto stash before rebase of "UsersTable" onto "origin/develop"
    
    * Adding balance
    
    * Format balance
    
    ---------
    
    Co-authored-by: Vincent Rainaud <vrainaud.pro@gmail.com>

--- next-app/src/app/administration/dashboard/page.tsx
+++ next-app/src/app/administration/dashboard/page.tsx
@@ -1,7 +1,23 @@
 "use client";
+import { Token } from "@/app/tokens/token.model";
+import UsersTable from "@/components/administration/dashboard/UsersTable";
+import { getTokens } from "@/hook/tokens.hook";
+import { getUsers } from "@/hook/users.hook";
+import { Hook, User } from "@/types/hookResponse.type";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const DashboardPage: React.FC = () => {
+  const { data: users, isLoading: isUsersLoading } = useQuery<Hook<User[]>>({
+    queryKey: ["users"],
+    queryFn: getUsers,
+  });
+
+  const { data: tokensFetch = [], isLoading: isTokensLoading } = useQuery<Token[]>({
+    queryKey: ["tokens"],
+    queryFn: getTokens,
+  });
+
   return (
     <>
       <header className="flex h-14 lg:h-[60px] items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40">
@@ -10,7 +26,12 @@ const DashboardPage: React.FC = () => {
         </div>
       </header>
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
-        
+        <UsersTable
+          users={users ? users?.data : []}
+          isUsersLoading={isUsersLoading}
+          tokens={tokensFetch ? tokensFetch : []}
+          isTokensLoading={isTokensLoading}
+        />
       </main>
     </>
   );
--- next-app/src/utils/number.utils.ts
+++ next-app/src/utils/number.utils.ts
@@ -1,12 +1,22 @@
-export function formatBigNumber(reserve) {
-  let number = Number(reserve);
+import { ethers } from "ethers";
 
-  // Conversion en millions avec une précision décimale appropriée
-  const million = 1000000;
-  const formattedNumber = (number / million).toLocaleString("en-US", {
-    minimumFractionDigits: 1,
-    maximumFractionDigits: 1,
-  });
+export function formatBigNumber(value: number) {
+  if (value === null || value === undefined) {
+    return "-";
+  }
 
-  return formattedNumber + "M";
+  value = String(value);
+  value = ethers.formatEther(value);
+
+  if (value >= 1e15) {
+    return `${(value / 1e15).toFixed(2)} Q`; // Quadrillions
+  } else if (value >= 1e12) {
+    return `${(value / 1e12).toFixed(2)} T`; // Trillions
+  } else if (value >= 1e9) {
+    return `${(value / 1e9).toFixed(2)} B`; // Billions
+  } else if (value >= 1e6) {
+    return `${(value / 1e6).toFixed(2)} M`; // Millions
+  } else {
+    return value.toFixed(2); // Small values
+  }
 }
